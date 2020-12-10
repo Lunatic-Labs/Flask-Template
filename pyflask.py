cab831e9262068c5904a82560de4362ae794090d
Brycejax <bryce.martin726@hotmail.com>
2020-12-10 13:12:16 -0600

    added a contact starter HTML file and added it to our python file. Works correctly.

--- pyflask.py
+++ pyflask.py
@@ -25,5 +25,10 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/contact/')
+
+def contact():
+    return render_template("contact.html")
+
 if __name__=="__main__":
     app.run(debug=True)
